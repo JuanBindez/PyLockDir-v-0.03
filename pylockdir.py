23c5cb5d7e1497d8f048fea16b4c85b403f2e3ac
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-07-14 15:08:34 -0300

    Update pylockdir.py

--- pylockdir.py
+++ pylockdir.py
@@ -21,6 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
+
 Author: www.github.com/JuanBindez
 License: https://github.com/JuanBindez/PyLockDir/blob/main/LICENSE
 Description: change file and directory permissions
