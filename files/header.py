280074afd4b92ff425f4e66695ce7c09edbe6f1e
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-07-17 18:24:25 -0300

    Update header.py

--- files/header.py
+++ files/header.py
@@ -34,14 +34,21 @@ OS: Linux
 
 
 class Color():
-        VERDE = '\033[92m'
-        VERMELHO = '\033[91m'
-        AMARELO = '\033[93m'
-        AZUL = '\033[1;34m'
-        MAGENTA = '\033[1;35m'
-        VERDE_CLARO = '\033[1;92m'
-        NEGRITO = '\033[;1m'
-        RESET = '\033[0m'
+    VERDE = '\033[92m'
+    VERDE_CLARO = '\033[1;92m'
+    VERMELHO = '\033[91m'
+    AMARELO = '\033[93m'
+    AZUL = '\033[1;34m'
+    MAGENTA = '\033[1;35m'
+    NEGRITO = '\033[;1m'
+    CYANO = '\033[1;36m'
+    CYANO_CLARO = '\033[1;96m'
+    CINZA_CLARO = '\033[1;37m'
+    CINZA_ESCURO = '\033[1;90m'
+    PRETO = '\033[1;30m'
+    BRANCO = '\033[1;97m'
+    INVERTE = '\033[;7m'
+    RESET = '\033[0m'
 
         
 def header_menu():
